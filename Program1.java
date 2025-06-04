290fc606c0026671b692e32d887f586d78e63d4e
Raj Yadav <raajyadavnp@gmail.com>
2025-06-04 19:58:13 +0530

    commit-3

--- Program1.java
+++ Program1.java
@@ -2,8 +2,10 @@ class Program1
 {
   public static void main(String[] args)
    {
-   System.out.println("Hello");
-   System.out.println("Hello World");
+   System.out.println("Hello 1");
+   System.out.println("Hello 2");
+   System.out.println("Hello 3");
+   System.out.println("Hello 4");
 
    }
 
