81592dcc6259ba8499d7fe6288f0f2f428545931
Raj Yadav <raajyadavnp@gmail.com>
2025-06-06 22:35:00 +0530

    commit Program1

--- Program1.java
+++ Program1.java
@@ -6,6 +6,8 @@ class Program1
    System.out.println("Hello 2");
    System.out.println("Hello 3");
    System.out.println("Hello 4");
+   System.out.println("Hello 5");
+   System.out.println("Hello 6");
 
    }
 
